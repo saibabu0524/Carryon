1440bd1a4baf195a12f9a39768e7de720c989b9c
saibabu0524 <saibabu2936@gmail.com>
2025-02-20 09:41:20 +0530

    updated the auth navgraph

--- feature/auth/src/main/java/com/saibabui/auth/presentation/navigation/AuthNavGraph.kt
+++ feature/auth/src/main/java/com/saibabui/auth/presentation/navigation/AuthNavGraph.kt
@@ -6,18 +6,28 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import com.saibabui.auth.presentation.ui.dashboard.DashBoardScreen
 import com.saibabui.auth.presentation.ui.login.LoginRoute
+import com.saibabui.auth.presentation.ui.verify.VerifyMobileNumberScreen
 
 
 fun NavGraphBuilder.authNavGraph(navController: NavHostController) {
     navigation(
         route = Graph.AUTHENTICATION,
-        startDestination = AuthScreen.Login.route
+        startDestination = AuthScreen.Dashboard.route
     ) {
         composable(AuthScreen.Login.route) {
-            LoginRoute()
+            LoginRoute(
+                navController
+            )
         }
         composable(AuthScreen.SignUp.route) {
+        }
+        composable(AuthScreen.Dashboard.route) {
+            DashBoardScreen(navController = navController)
+        }
+        composable(AuthScreen.VerifyMobileNumber.route) {
+            VerifyMobileNumberScreen(navController)
         }
     }
 }
--- feature/auth/src/main/java/com/saibabui/auth/presentation/navigation/AuthRoutes.kt
+++ feature/auth/src/main/java/com/saibabui/auth/presentation/navigation/AuthRoutes.kt
@@ -4,6 +4,8 @@ package com.saibabui.auth.presentation.navigation
 sealed class AuthScreen(val route: String) {
     data object Login : AuthScreen("login")
     data object SignUp : AuthScreen("signup")
+    data object Dashboard : AuthScreen("dashboard")
+    data object VerifyMobileNumber : AuthScreen("verify_mobile_number")
 }
 
 sealed class Home(val route: String) {
