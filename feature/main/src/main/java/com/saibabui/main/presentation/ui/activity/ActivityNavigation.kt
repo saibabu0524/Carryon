dd9e7203aabf7215e4efa14bcca9cfa5a2bf1963
saibabu0524 <saibabu2936@gmail.com>
2025-02-24 22:20:45 +0530

    added routes and navigation constants

---
+++ feature/main/src/main/java/com/saibabui/main/presentation/ui/activity/ActivityNavigation.kt
@@ -0,0 +1,8 @@
+package com.saibabui.main.presentation.ui.activity
+
+import androidx.navigation.NavController
+import com.saibabui.main.navigation.Home
+
+
+
+fun NavController.navigateToActivityScreen() = navigate(Home.ActivityDestination)
