eeeeb7a4123bb5df6bf43691b378f6dd0e04d80b
saibabu0524 <saibabu2936@gmail.com>
2025-03-03 09:24:58 +0530

    added routes and navigation constants

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.compose.compiler)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -78,4 +80,10 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.androidx.navigation.compose)
 
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
+}
+
+kapt {
+    correctErrorTypes = true
 }
--- feature/main/src/main/java/com/saibabui/main/navigation/BottomNavigationModel.kt
+++ feature/main/src/main/java/com/saibabui/main/navigation/BottomNavigationModel.kt
@@ -9,16 +9,9 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.saibabui.home.R
 
-data class BottomNavigationModel(
-    val route: String,
-    val label: String,
-    val icon: Painter
-)
-
-
-enum class MainNavDestination(val icon: ImageVector, val resId: Int, val route: Home) {
-    HOME(icon = Icons.Default.Home, resId = R.string.home_nav_lable, route = Home.HomeScreen),
-    ACTIVITY(icon = Icons.Default.AccessTimeFilled, resId = R.string.activity_nav_lable, route = Home.Activity),
-    TRANSACTIONS(icon = Icons.Default.History, resId = R.string.transactions_nav_lable, route = Home.Transactions),
-    PROFILE(icon = Icons.Default.Person, resId = R.string.profile_nav_lable, route = Home.ProfileScreen)
+enum class MainNavDestination(val icon: ImageVector, val resId: Int, val destination: Home) {
+    HOME(icon = Icons.Default.Home, resId = R.string.home_nav_lable, destination = Home.HomeScreenDestination),
+    ACTIVITY(icon = Icons.Default.AccessTimeFilled, resId = R.string.activity_nav_lable, destination = Home.ActivityDestination),
+    TRANSACTIONS(icon = Icons.Default.History, resId = R.string.transactions_nav_lable, destination = Home.TransactionsDestination),
+    PROFILE(icon = Icons.Default.Person, resId = R.string.profile_nav_lable, destination = Home.ProfileScreenDestination)
 }
