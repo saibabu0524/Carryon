32371bda47c8b732f5b09bde3879af7ff6e40feb
saibabu0524 <saibabu2936@gmail.com>
2025-04-03 09:25:48 +0530

    updated the profile UI

---
+++ core/ui/src/main/java/com/saibabui/ui/Buttons.kt
@@ -0,0 +1,56 @@
+package com.saibabui.ui
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun PrimaryButton(buttonText: String, onClick: () -> Unit, modifier: Modifier = Modifier) {
+    Button(
+        onClick = onClick,
+        shape = RoundedCornerShape(5.dp),
+        modifier = modifier
+    ) {
+        Text(
+            text = buttonText,
+            fontFamily = MaterialTheme.typography.bodyMedium.fontFamily,
+            fontWeight = FontWeight.Medium,
+            fontSize = 16.sp,
+            lineHeight = 16.sp,
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+
+@Composable
+fun SecondaryButton(buttonText: String, onClick: () -> Unit, modifier: Modifier = Modifier) {
+    Button(
+        onClick = onClick,
+        shape = RoundedCornerShape(5.dp),
+        modifier = modifier,
+        colors = ButtonColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+            contentColor = MaterialTheme.colorScheme.onSurface,
+            disabledContainerColor = MaterialTheme.colorScheme.surface,
+            disabledContentColor = MaterialTheme.colorScheme.onSurface
+        )
+    ) {
+        Text(
+            text = buttonText,
+            fontFamily = MaterialTheme.typography.bodyMedium.fontFamily,
+            fontWeight = FontWeight.Medium,
+            fontSize = 16.sp,
+            lineHeight = 16.sp,
+            textAlign = TextAlign.Center,
+        )
+    }
+}
