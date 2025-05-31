5ef80028047cfaf13806eaf4991f1e61f25e6b27
saibabu0524 <saibabu2936@gmail.com>
2025-05-31 23:46:03 +0530

    accounts screen added

---
+++ core/datastore/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "com.saibabui.datastore"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+
+    implementation("androidx.datastore:datastore-preferences:1.1.4")
+
+    //dagger hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+}
--- feature/main/src/main/java/com/saibabui/main/navigation/BottomNavigation.kt
+++ feature/main/src/main/java/com/saibabui/main/navigation/BottomNavigation.kt
@@ -15,19 +15,28 @@ import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
+import androidx.navigation.NavOptions
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.saibabui.main.presentation.ui.activity.TemplateScreen
 import com.saibabui.main.presentation.ui.home.HomeScreen
 import com.saibabui.main.presentation.ui.pdfviewer.PdfViewerScreen
-
 import com.saibabui.main.presentation.ui.profile.ProfileScreen
-import com.saibabui.main.presentation.ui.transactions.TransactionScreen
+import com.saibabui.main.presentation.ui.resumedetails.ResumeForm
+import com.saibabui.main.presentation.ui.resumedetails.ResumeFormScreen
+import com.saibabui.main.presentation.ui.resumedetails.navigateToResumeFormNavigation
+
+import com.saibabui.main.presentation.ui.templates.TemplatesScreen
+import com.saibabui.main.presentation.ui.transactions.RecentResumesScreen
+import com.saibabui.main.utils.PDFViewerScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreenWithBottomNavigation(navController: NavHostController) {
+fun HomeScreenWithBottomNavigation(
+    navController: NavHostController,
+    onNavigateToLogout: suspend () -> Unit = {}
+) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentDestination = navBackStackEntry?.destination
     Scaffold(
@@ -71,18 +80,41 @@ fun HomeScreenWithBottomNavigation(navController: NavHostController) {
                 HomeScreen(navController)
             }
             composable(route = Home.ProfileScreenDestination.route) {
-                ProfileScreen(navController)
+//                ProfileScreen(navController)
+                ProfileScreen(onNavigateToLogout = onNavigateToLogout)
             }
             composable(route = Home.ActivityDestination.route) {
                 TemplateScreen(navController)
             }
             composable(route = Home.TransactionsDestination.route) {
-                TransactionScreen(navController)
+                RecentResumesScreen(navController)
             }
             composable<ResumePreviewScreen> {
-                PdfViewerScreen(
-
+                PDFViewerScreen(
+                    pdfUrl = "https://www.overleaf.com/latex/templates/rendercv-engineeringresumes-theme/shwqvsxdgkjy.pdf"
                 )
+//                PdfViewerScreen(
+//
+//                )
+            }
+            composable<TemplatesScreen> {
+                TemplatesScreen {
+                    navController.navigate(ResumeFormScreen) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                    }
+                }
+            }
+            composable<ResumeFormScreen> {
+                ResumeForm {
+                    navController.navigateToResumeFormNavigation(
+                        navOptions = NavOptions.Builder()
+                            .setPopUpTo(navController.graph.findStartDestination().id, true)
+                            .build()
+                    )
+                }
             }
         }
     }
